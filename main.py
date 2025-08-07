1bedae1259214b4b0bfed278f3e7a2e44c0b1c42
JOHNZEL MARAHAY <fstack.jz@gmail.com>
2025-08-07 21:53:59 +0800

    changes from day 1

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-# this is a test
+def addition(x, y):
+    return x +  y
+
+def subtraction(x, y):
+    return x - y
+
+def multiplication(x, y):
+    return x * y 
+
+def division(x, y):
+    if division 
