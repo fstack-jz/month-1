6ee6cf0206e159a0f40139f3f846ae700a2f3563
JOHNZEL MARAHAY <fstack.jz@gmail.com>
2025-08-09 21:58:22 +0800

    update
    updated ver--

--- main.py
+++ main.py
@@ -11,24 +11,36 @@ def tpwrtr(text, delay=0.09):
         t.sleep(delay)
     print()
 
-def main():
-    name = input("What is your name?\n"
-                 ": ").upper()
-    while True:
-        age = input("What is your age?\n" \
-        ": ")
-        if age.isdigit():
-            break
-        else:
-            print("Please enter a number only.")
-    occupation = input("Enter your current occupation\n"
-                       ": ").upper()
-    place_of_work_or_study = input("In?\n"
-                                   ": ").upper()
+
+name = input("What is your name?\n"
+             ": ").upper()
+while True:
+    age = input("What is your age?\n" \
+    ": ")
+    if age.isdigit():
+        break
+    else:
+        print("Please enter a number only.")
+occupation = input("Enter your current occupation\n"
+                    ": ").upper()
+place_of_work_or_study = input("In?\n"
+                                ": ").upper()
     
+    
+print("-----User Interface-----")
+print(f"\nGreetings!, {name}!, you are currently {age} and a {occupation} in {place_of_work_or_study}\n")
+print("------------------------")
+    
+#result
+user_name = name
+user_age = age
+authorizeAccess = occupation
+workplace = place_of_work_or_study
 
-    print("-----User Interface-----")
-    print(f"\nGreetings!, {name}!, you are currently {age} and a {occupation} in {place_of_work_or_study}\n")
-    print("------------------------")
 
-main()
+def UI(delay = 2.5):
+    print("Please wait....")
+    t.sleep(delay)
+    print(f"Welcome!, {user_name}!")
+
+UI()
